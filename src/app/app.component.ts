296b29fc33504926ed266c477b9181b8b26aa873
Juanma <jmmolins87@gmail.com>
2023-02-16 17:32:09 +0100

    fixed piano app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'emptyangularproject';
+  
+  applySound( num: number ) {
+    const audio = new Audio();
+    audio.src = `./../assets/sounds/note${ num }.wav`;
+    audio.load();
+    audio.play();
+  }
+
 }
